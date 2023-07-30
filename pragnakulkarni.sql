f04221c2fdbb4781a00974a45b0ad9040ea1ba3e
pragnakulkarni <kulakarnisaipragna666@gmail.com>
2023-07-30 16:05:50 +0000

    again changed

--- pragnakulkarni.sql
+++ pragnakulkarni.sql
@@ -4,25 +4,15 @@ create index ind_pragna on FACEBOOK (USERID,DOB_YEAR,AGE,GENDER);
 SELECT COUNT(USERID) FROM FACEBOOK WHERE DOB_YEAR=1999;
 
 --Write a SQL query to count the total number of social media users for each age group?
-SELECT AGE,COUNT(USERID) FROM FACEBOOK GROUP BY AGE ORDER BY AGE;
+SELECT AGE,COUNT(USERID) FROM FACEBOOK GROUP BY AGE;
 
 --Write a SQL query to find total users in social media above age 15?
 SELECT COUNT(USERID) FROM FACEBOOK WHERE AGE>15;
 
 --4.	Write a SQL query to find the percentage of female users who are using social media?
-WITH FEMALE_USERS (FEMALE_COUNT) AS 
-(SELECT COUNT(*) FROM FACEBOOK WHERE GENDER='female'),
-TOTAL_USERS (TOTAL_COUNT) AS(SELECT COUNT(*) FROM FACEBOOK)
-
-SELECT
-  (FEMALE_USERS.FEMALE_COUNT / TOTAL_USERS.TOTAL_COUNT)*100 AS percentage
-FROM total_users, FEMALE_USERS;
+select 100.0 * COUNT(case when GENDER ='female' then 1 else null end)/
+ COUNT(USERID) as percentage FROM FACEBOOK;
 
 --Write a SQL query to find the percentage of female users among users above the age of 17?
-WITH FEMALE_USERS (FEMALE_COUNT) AS 
-(SELECT COUNT(*) FROM FACEBOOK WHERE GENDER='female' AND AGE>17),
-TOTAL_USERS (TOTAL_COUNT) AS(SELECT COUNT(*) FROM FACEBOOK WHERE AGE>17)
-
-SELECT
-  (FEMALE_USERS.FEMALE_COUNT / TOTAL_USERS.TOTAL_COUNT)*100 AS percentage
-FROM total_users, FEMALE_USERS;
+select 100.0 * COUNT(case when GENDER ='female' then 1 else null end)/
+ COUNT(USERID) as percentage FROM FACEBOOK where age >17;
